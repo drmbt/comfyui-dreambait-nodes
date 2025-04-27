070860ca8664d80a8057f4fe09b5855a2ccb50c2
drmbt <dreambaitrecordings@gmail.com>
2025-04-26 20:47:54 -0700

    add compare_image_sililarity node

--- src/nodes/TextPlusPlus.py
+++ src/nodes/TextPlusPlus.py
@@ -10,6 +10,14 @@ class TextPlusPlus:
     def INPUT_TYPES(cls):
         return {
             "required": {
+                "prepend": (
+                    "STRING",
+                    {
+                        "default": "", 
+                        "multiline": False, 
+                        "tooltip": "prepend this text to the body parameter separated by the delimiter"
+                    },
+                ),
                 "body": (
                     "STRING",
                     {
@@ -17,6 +25,14 @@ class TextPlusPlus:
                         "multiline": True,
                         "print_to_screen": True,
                     },
+                ),
+                "append": (
+                    "STRING",
+                    {
+                        "default": "", 
+                        "multiline": False, 
+                        "tooltip": "append this text to the body parameter separated by the delimiter"
+                    }
                 ),
                 "delimiter": (
                     "STRING",
@@ -28,35 +44,6 @@ class TextPlusPlus:
                     },
                 )
             },
-            "optional": {
-                "prepend": (
-                    "STRING",
-                    {
-                        "default": "", 
-                        "multiline": True, 
-                        "defaultInput": True,
-                        "tooltip": "prepend this text to the body parameter separated by the delimiter"
-                    },
-                ),
-                "override_body": (
-                    "STRING",
-                    {
-                        "default": "", 
-                        "multiline": True, 
-                        "defaultInput": True,
-                        "tooltip": "replace all text in the body parameter with text from this input"
-                    },
-                ),
-                "append": (
-                    "STRING",
-                    {
-                        "default": "", 
-                        "multiline": True, 
-                        "defaultInput": True,
-                        "tooltip": "append this text to the body parameter separated by the delimiter"
-                    }
-                )
-            },
         }
     RETURN_TYPES = ("STRING", "STRING", "STRING", "STRING")
     RETURN_NAMES = ("text", "prepend", "body", "append")
@@ -65,19 +52,21 @@ class TextPlusPlus:
     CATEGORY = "DRMBT nodes"
 
     @classmethod
-    def IS_CHANGED(cls, body, delimiter, prepend="", override_body="", append=""):
-        # All text inputs and the delimiter can affect the output
-        return hash((prepend, body, override_body, append, delimiter))
+    def IS_CHANGED(cls, body, prepend, append, delimiter):
+        # Use a more reliable change detection method than hash
+        return (prepend, body, append, delimiter)
 
-    def text_assemble(self, prepend="", body="", override_body=None, append="", delimiter=" "):
-        # Add handling of escape sequences
-        delimiter = bytes(delimiter, "utf-8").decode("unicode_escape")
-        
-        if override_body is not None:
-            body = override_body
-        if prepend != "":
-            prepend = f"{prepend}{delimiter}"
-        if append != "":
-            append = f"{delimiter}{append}" 
-        text = f"{prepend}{body}{append}"
-        return {"ui": {"text": text, "prepend": prepend, "body": body, "append": append}, "result": (text, prepend, body, append)}
+    def text_assemble(self, body="", prepend="", append="", delimiter=" "):
+        try:
+            # Add handling of escape sequences
+            delimiter = bytes(delimiter, "utf-8").decode("unicode_escape")
+            
+            if prepend != "":
+                prepend = f"{prepend}{delimiter}"
+            if append != "":
+                append = f"{delimiter}{append}" 
+            text = f"{prepend}{body}{append}"
+            return {"ui": {"text": text, "prepend": prepend, "body": body, "append": append}, 
+                   "result": (text, prepend, body, append)}
+        except UnicodeError as e:
+            raise ValueError(f"Invalid text or delimiter encoding: {str(e)}")
